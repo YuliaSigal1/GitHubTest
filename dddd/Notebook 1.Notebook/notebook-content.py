06deeac3a467cbae318754961315f791eba684ab
YuliaSigal1 <166691388+YuliaSigal1@users.noreply.github.com>
2025-02-11 10:27:01 +0200

    Committing 2 items from workspace 3bb07366-8657-481b-ac6c-46efb49079ac

---
+++ dddd/Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,24 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "6336260b-94bb-4868-9e01-2381917971e0",
+# META       "default_lakehouse_name": "",
+# META       "default_lakehouse_workspace_id": "",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "6336260b-94bb-4868-9e01-2381917971e0"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+

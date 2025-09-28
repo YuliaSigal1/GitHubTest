70c5257a3f138eecc2eab5714e233139403b50b1
YuliaSigal1 <166691388+YuliaSigal1@users.noreply.github.com>
2025-09-28 15:55:27 +0300

    Committing 29 items from workspace 4fc0a151-466e-4201-9ab0-4a7c34b4eb2a

---
+++ TestAllTypesArt/Notebook 2.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "1476f5be-03a0-4c87-932f-e17fac5f1e5f",
+# META       "default_lakehouse_name": "LakehouseTest",
+# META       "default_lakehouse_workspace_id": "4fc0a151-466e-4201-9ab0-4a7c34b4eb2a",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "1476f5be-03a0-4c87-932f-e17fac5f1e5f"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
---
+++ TestAllTypesArt/warehouse.Warehouse/dbo/Tables/Alphabetical_list_of_products.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [dbo].[Alphabetical_list_of_products] (
+
+	[ProductID] int NULL, 
+	[ProductName] varchar(8000) NULL, 
+	[SupplierID] int NULL, 
+	[CategoryID] int NULL, 
+	[QuantityPerUnit] varchar(8000) NULL, 
+	[UnitPrice] decimal(38,18) NULL, 
+	[UnitsInStock] smallint NULL, 
+	[UnitsOnOrder] smallint NULL, 
+	[ReorderLevel] smallint NULL, 
+	[Discontinued] bit NULL, 
+	[CategoryName] varchar(8000) NULL
+);

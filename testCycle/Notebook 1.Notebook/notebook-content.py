48b374afe7733a96da892d83a7f117b56e7ddef8
YuliaSigal1 <166691388+YuliaSigal1@users.noreply.github.com>
2025-12-03 12:33:11 +0200

    Committing 5 items from workspace f9c33da6-a7d9-43ff-b0e1-fbcf85044db7

---
+++ testCycle/Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "15437d1b-e7fa-4a20-acc0-d1b726eea2bb",
+# META       "default_lakehouse_name": "",
+# META       "default_lakehouse_workspace_id": "",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "15437d1b-e7fa-4a20-acc0-d1b726eea2bb"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }

d4bd3606f29cc5b3e6c3da84b28fbc986c7dbc9d
YuliaSigal1 <166691388+YuliaSigal1@users.noreply.github.com>
2026-01-13 11:12:16 +0200

    Committing 1 item from workspace 6716fa14-862c-4b14-9cd0-3024a7dd00c5

--- Test_rename/Notebook 1.Notebook/notebook-content.py
+++ Test_rename/Notebook 1.Notebook/notebook-content.py
@@ -9,7 +9,7 @@
 # META   "dependencies": {
 # META     "lakehouse": {
 # META       "default_lakehouse": "1f87022f-b239-4513-9eed-4b2b34560c30",
-# META       "default_lakehouse_name": "lakehouseTest_ren",
+# META       "default_lakehouse_name": "lakehouseTest_rename_2",
 # META       "default_lakehouse_workspace_id": "6716fa14-862c-4b14-9cd0-3024a7dd00c5",
 # META       "known_lakehouses": [
 # META         {
